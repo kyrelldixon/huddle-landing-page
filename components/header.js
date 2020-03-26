af68a0f03fe03cd0985fdbb9d2127dc6c74aa47a
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-03-26 11:58:26 -0500

    feat: finish header styles

--- components/header.js
+++ components/header.js
@@ -1,14 +1,14 @@
 export default () => (
   <header className="relative bg-center bg-no-repeat bg-cover bg-cyan-light bg-mobile lg:bg-desktop">
-    <nav className="flex items-center justify-between px-4 py-6 lg:px-12 lg:mb-4">
+    <nav className="flex items-center justify-between px-4 py-6 md:px-8 lg:px-12 lg:mb-4">
       <img className="h-4" src="/images/logo.svg" alt="Huddle Logo" />
       <button className="px-6 py-1 text-xs font-bold bg-white rounded-full shadow-lg">
         Try It Free
       </button>
     </nav>
-    <div className="flex flex-col items-center px-8 pt-16 pb-8 text-center lg:pb-12 lg:px-12 lg:pt-4 lg:text-left lg:flex-row">
-      <div className="max-w-sm mb-4 lg:mb-0 lg:mr-12">
-        <h1 className="mb-8 text-2xl font-bold lg:text-3xl">
+    <div className="flex flex-col items-center px-8 pt-16 pb-8 text-center md:pb-12 md:pt-8 md:text-left md:flex-row lg:pb-12 lg:px-12 lg:pt-4 lg:text-left lg:flex-row">
+      <div className="pr-8 mb-4 md:mb-0 md:mr-8 lg:mr-12">
+        <h1 className="mb-8 text-3xl font-bold">
           Build The Community Your Fans Will Love
         </h1>
         <p className="mb-8 text-base">
@@ -16,7 +16,7 @@ export default () => (
           so does your audience. Create connections with your users as you
           engage in genuine discussion.
         </p>
-        <button className="px-16 py-3 mb-8 text-xs font-bold text-white rounded-full bg-pink lg:mb-0 lg:py-4">
+        <button className="px-16 py-3 mb-8 text-xs font-bold text-white rounded-full bg-pink md:mb-0 lg:py-4">
           Get Started For Free
         </button>
       </div>
