710cacd8daa3397826454fcb5cc7dbb460de84ea
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-03-25 23:03:46 -0500

    feat: add styling based on style guide

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/index.css'
+import Head from "next/head";
+import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "cyan-light": "hsl(193, 100%, 96%)",
+        "cyan-dark": "hsl(192, 100%, 9%)",
+        blue: "hsl(208, 11%, 55%)",
+        pink: "hsl(322, 100%, 66%)"
+      },
+      fontFamily: {
+        body: ["'Poppins'", "sans-serif"],
+        heading: ["'Open Sans'", "sans-serif"]
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
