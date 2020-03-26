2a4c8446185b4eabe0367551fa7bc5f4f9a3dbf7
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-03-26 10:59:18 -0500

    feat: update styling and add social icons

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,10 @@
-import Logo from './logo'
+import { TiSocialFacebook, TiSocialTwitter } from "react-icons/ti";
+import { IoLogoInstagram } from "react-icons/io";
+import Logo from "./logo";
 
 export default () => (
-  <footer className="bg-cyan-dark text-white px-4 pt-24 pb-8">
-    <div className="mb-8">
+  <footer className="px-4 pt-24 pb-8 text-white bg-cyan-dark">
+    <div className="mb-16">
       <Logo />
       <div className="flex mb-8">
         <img
@@ -10,21 +12,21 @@ export default () => (
           src="/images/icon-location.svg"
           alt="location icon"
         />
-        <p className="ml-6 leading-tight -mt-1">
+        <p className="ml-6 -mt-1 leading-tight">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua
         </p>
       </div>
       <div className="flex mb-8">
         <img className="h-4" src="/images/icon-phone.svg" alt="phone icon" />
-        <p className="ml-6 leading-tight -mt-1">+1-543-123-4567</p>
+        <p className="ml-6 -mt-1 leading-tight">+1-543-123-4567</p>
       </div>
       <div className="flex mb-8">
         <img className="h-4" src="/images/icon-email.svg" alt="email icon" />
-        <p className="ml-6 leading-tight -mt-1">example@huddle.com</p>
+        <p className="ml-6 -mt-1 leading-tight">example@huddle.com</p>
       </div>
     </div>
-    <nav className="mb-8">
+    <nav className="mb-16">
       <ul className="leading-loose">
         <li>About Us</li>
         <li>What We Do</li>
@@ -34,10 +36,33 @@ export default () => (
         <li>Contact Us</li>
       </ul>
     </nav>
-    <small className="block text-center mx-auto mb-1">
-      &copy; Copyright 2018 Huddle. All rights reserved.
+    <div className="flex justify-center mb-8 text-lg">
+      <a
+        href="https://www.twitter.com/kyrelldixon"
+        target="_blank"
+        className="px-1 py-1 mr-4 border border-gray-100 rounded-full"
+      >
+        <TiSocialFacebook size="1.3rem" />
+      </a>
+      <a
+        href="https://www.twitter.com/kyrelldixon"
+        target="_blank"
+        className="px-1 py-1 mr-4 border border-gray-100 rounded-full"
+      >
+        <TiSocialTwitter size="1.3rem" />
+      </a>
+      <a
+        href="https://www.instagram.com/kyrell.dixon"
+        target="_blank"
+        className="px-1 py-1 border border-gray-100 rounded-full"
+      >
+        <IoLogoInstagram size="1.3rem" />
+      </a>
+    </div>
+    <small className="block mx-auto mb-2 text-center">
+      &copy; Copyright {new Date().getFullYear()} Huddle. All rights reserved.
     </small>
-    <p className="text-center text-sm">
+    <p className="text-xs text-center">
       Challenge by{" "}
       <a
         href="https://www.frontendmentor.io?ref=challenge"
