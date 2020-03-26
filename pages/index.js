83c126b52cb430bcb2235a0bb8d7614504a04efe
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-03-25 23:54:11 -0500

    feat: add mobile hero

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,53 @@
-export default () => <div></div>;
+export default () => (
+  <div className="font-body text-cyan-dark">
+    <header
+      className="relative bg-cyan-light bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url('/images/bg-hero-mobile.svg')` }}
+    >
+      <nav className="px-4 py-6 flex items-center justify-between">
+        <img className="h-4" src="/images/logo.svg" alt="Huddle Logo" />
+        <button className="bg-white px-6 py-1 text-xs font-bold rounded-full shadow-lg">
+          Try It Free
+        </button>
+      </nav>
+      <div className="flex flex-col items-center pt-16 text-center px-8 pb-8">
+        <h1 className="text-2xl font-bold mb-8">
+          Build The Community Your Fans Will Love
+        </h1>
+        <p className="mb-8 text-base">
+          Huddle re-imagines the way we build communities. You have a voice, but
+          so does your audience. Create connections with your users as you
+          engage in genuine discussion.
+        </p>
+        <button className="bg-pink text-xs text-white mb-8 px-16 py-3 font-bold rounded-full">
+          Get Started For Free
+        </button>
+        <img
+          src="/images/illustration-mockups.svg"
+          alt="desktop and mobile browsers"
+        />
+      </div>
+    </header>
+    Grow Together Generate meaningful discussions with your audience and build a
+    strong, loyal community. Think of the insightful conversations you miss out
+    on with a feedback form. Flowing Conversations You wouldn't paginate a
+    conversation in real life, so why do it online? Our threads have
+    just-in-time loading for a more natural flow. Your Users It takes no time at
+    all to integrate Huddle with your app's authentication solution. This means,
+    once signed in to your app, your users can start chatting immediately. Ready
+    To Build Your Community? Get Started For Free Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+    dolore magna aliqua +1-543-123-4567 example@huddle.com About Us What We Do
+    FAQ Career Blog Contact Us &copy; Copyright 2018 Huddle. All rights
+    reserved.
+    <footer>
+      <p class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </p>
+    </footer>
+  </div>
+);
